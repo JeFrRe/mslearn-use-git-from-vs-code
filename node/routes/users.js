2a266fa77177bced99ab4a83c455ded68a632afe
JFRenard <renard.jfry@gmail.com>
2020-12-21 18:19:07 +0100

    NEW3

--- node/routes/users.js
+++ node/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource NEW2');
+  res.send('respond with a resource NEW3');
 });
 
 module.exports = router;
