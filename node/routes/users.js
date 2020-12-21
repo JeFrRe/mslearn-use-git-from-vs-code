497cceb2a90399efadfc2a1dfba611528de24c45
JFRenard <renard.jfry@gmail.com>
2020-12-21 18:15:38 +0100

    Putain de message

--- node/routes/users.js
+++ node/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource NEW');
+  res.send('respond with a resource NEW2');
 });
 
 module.exports = router;
